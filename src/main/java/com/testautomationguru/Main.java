f6cc20990db9760239be13b44d7cc8f60faa3817
Selvaraj, Vinoth <Vinoth.Selvaraj@pearson.com>
2017-01-17 11:47:12 -0600

    command line option added for setting image path

--- src/main/java/com/testautomationguru/Main.java
+++ src/main/java/com/testautomationguru/Main.java
@@ -14,12 +14,18 @@ public final class Main {
 		}else{
 			PDFUtil pdfutil = new PDFUtil();
 			pdfutil.setCompareMode(CompareMode.VISUAL_MODE);
+			
+			if(args.length>2){
+				pdfutil.highlightPdfDifference(true);
+				pdfutil.setImageDestinationPath(args[2]);				
+			}
+
 			pdfutil.compare(args[0], args[1]);
 		}
 		
 	}
 
 	private static void showUsage(){
-		System.out.println("Usage: java -jar pdf-util.jar file1.pdf file2.pdf");
+		System.out.println("Usage: java -jar pdf-util.jar file1.pdf file2.pdf [Optional:image-destination-path]");
 	}
 }
