d8b2f2b1e634da9aaec9624edcd196e25c1cb8f4
Mathias Ver Elst <mathias.ver.elst@gmail.com>
2017-12-29 21:53:50 +0100

    Added support for Streams

--- src/main/java/com/testautomationguru/utility/ImageUtil.java
+++ src/main/java/com/testautomationguru/utility/ImageUtil.java
@@ -1,19 +1,18 @@
 package com.testautomationguru.utility;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import javax.imageio.ImageIO;
-
 class ImageUtil {
-	
-	static Logger logger = Logger.getLogger(ImageUtil.class.getName());
-	
-	static boolean compareAndHighlight(final BufferedImage img1, final BufferedImage img2, String fileName, boolean highlight, int colorCode) throws IOException {
 
-	    final int w = img1.getWidth();
+	static Logger logger = Logger.getLogger(ImageUtil.class.getName());
+
+	static boolean compareAndHighlight(final BufferedImage img1, final BufferedImage img2, String fileName, PdfUtilImageListener imageListener, boolean highlight, int colorCode) throws IOException {
+
+		final int w = img1.getWidth();
 	    final int h = img1.getHeight();
 	    final int[] p1 = img1.getRGB(0, 0, w, h, null, 0, w);
 	    final int[] p2 = img2.getRGB(0, 0, w, h, null, 0, w);
@@ -24,11 +23,11 @@ class ImageUtil {
 	    	    for (int i = 0; i < p1.length; i++) {
 	    	        if (p1[i] != p2[i]){
 	    	            p1[i] = colorCode;
-	    	        } 
+	    	        }
 	    	    }
 	    	    final BufferedImage out = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 	    	    out.setRGB(0, 0, w, h, p1, 0, w);
-	    	    saveImage(out, fileName);
+				imageListener.imageGenerated(out, fileName);
 	    	}
 	    	return false;
 	    }
@@ -38,9 +37,9 @@ class ImageUtil {
 	static void saveImage(BufferedImage image, String file){
 		try{
 			File outputfile = new File(file);
-			ImageIO.write(image, "png", outputfile);	
-		}catch(Exception e){
+			ImageIO.write(image, "png", outputfile);
+		} catch(Exception e){
 			e.printStackTrace();
 		}
-	}	
+	}
 }
---
+++ src/main/java/com/testautomationguru/utility/PdfUtilImageListener.java
@@ -0,0 +1,7 @@
+package com.testautomationguru.utility;
+
+import java.awt.image.BufferedImage;
+
+public interface PdfUtilImageListener {
+    void imageGenerated(BufferedImage bufferedImage, String fileName);
+}
