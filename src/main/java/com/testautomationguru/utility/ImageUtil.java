74a20249614fff11648a974c06082beee487bb18
Pablo Nicolas Diaz <pablonicolas.diaz@gmail.com>
2019-03-05 12:32:35 -0300

    Refactoring : variable declarations were replaced using lombok.val. replaced Closeable.close with lombok.Cleanup

--- src/main/java/com/testautomationguru/utility/ImageUtil.java
+++ src/main/java/com/testautomationguru/utility/ImageUtil.java
@@ -7,16 +7,18 @@ import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
+import lombok.val;
+
 class ImageUtil {
 	
 	static Logger logger = Logger.getLogger(ImageUtil.class.getName());
 	
 	static boolean compareAndHighlight(final BufferedImage img1, final BufferedImage img2, String fileName, boolean highlight, int colorCode) throws IOException {
 
-	    final int w = img1.getWidth();
-	    final int h = img1.getHeight();
-	    final int[] p1 = img1.getRGB(0, 0, w, h, null, 0, w);
-	    final int[] p2 = img2.getRGB(0, 0, w, h, null, 0, w);
+	    val w = img1.getWidth();
+	    val h = img1.getHeight();
+	    val p1 = img1.getRGB(0, 0, w, h, null, 0, w);
+	    val p2 = img2.getRGB(0, 0, w, h, null, 0, w);
 
 	    if(!(java.util.Arrays.equals(p1, p2))){
 	    	logger.warning("Image compared - does not match");
@@ -26,7 +28,7 @@ class ImageUtil {
 	    	            p1[i] = colorCode;
 	    	        } 
 	    	    }
-	    	    final BufferedImage out = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+	    	    val out = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 	    	    out.setRGB(0, 0, w, h, p1, 0, w);
 	    	    saveImage(out, fileName);
 	    	}
@@ -37,7 +39,7 @@ class ImageUtil {
 
 	static void saveImage(BufferedImage image, String file){
 		try{
-			File outputfile = new File(file);
+			val outputfile = new File(file);
 			ImageIO.write(image, "png", outputfile);	
 		}catch(Exception e){
 			e.printStackTrace();
